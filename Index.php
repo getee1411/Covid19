9fc7837120374e43e1ea8840f19c53f702c601fe
getee1411 <62593648+getee1411@users.noreply.github.com>
2020-04-25 09:22:34 +0700

    halaman awal

---
+++ Index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+
+<?php
+    include 'koneksi/config.php';
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SELF CHECK UP</title>
+
+    <link rel="stylesheet" href="css/style1.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+</head>
+<body>
+    <nav class="navbar">
+        <a href="#" class="logo">SELF CHECK UP</a>
+        <ul class="nav">
+            <li><a href="#home">BERANDA</a></li>
+            <li><a href="#about">TENTANG</a></li>
+            <li><a href="#profil">PROFIL</a></li>
+            <li><a href="start.php" class="btn">CHECK UP</a></li>
+        </ul>
+    </nav>
+    <div class="banner-area" id="home">
+        <img class="wave" src="img/wave2.png" alt="">
+            <div class="container">
+                <div class="img">
+                    <img src="img/doctorr.svg" alt="">
+                </div>
+                <div class="container-reg">
+                    <h2>AYO CEK KESEHATAN</h2>
+                    <h4>Ciptakan Lingkungan Aman</h4>
+                    <p>
+                        Semakin meningkatnya kasus ODP, PDP, dan positif COVID-19 di Jawa Timur,
+                        tentunya self-assesment dibutuhkan untuk mengecek kesehatan pribadi.
+                    </p>
+                    <a class="btn" href="start.php">MULAI CHECK UP</a>
+                </div>
+            </div>
+    </div>
+    <div class="about-area" id="about">
+        <div class="container2">
+            <h3>SELF ASSESMENT</h3>
+            <p>
+                Self Assesment ini adalah alat sederhana yang bisa membantu menentukan apakah
+                anda sehat-sehat saja <br>
+                atau ada gejala yang memerlukan
+                penilaian, pemeriksaan, <br>
+                dan pengujian lebih lanjut 
+                untuk COVID 19
+            </p>
+        </div>
+        <div class="container3">
+        <h3>SELF PRE CHECK UP</h3>
+            <div class="container4">
+                <div class="card">
+                    <div class="img2">
+                        <img src="img/check.svg" alt="">
+                    </div>
+                    <h4>SIAPA YANG HARUS TES?</h4>
+                    <div class="cardP">
+                    <p>• &ensp; Pasien yang dirawat di rumah sakit, atau kemungkinan &emsp; &nbsp; dirawat di rumah sakit <br>
+•	&ensp; Pekerja Perawatan Kesehatan <br>
+•	&ensp; Penghuni fasilitas perawatan jangka panjang <br>
+•	&ensp; Bagian dari penyelidikan cluster atau wabah <br>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img2">
+                        <img src="img/safe.svg" alt="">
+                    </div>
+                    <h4>SIAPA YANG TIDAK PERLU TES?</h4>
+                    <div class="cardP">
+                    <p>• &ensp;	Orang tanpa gejala <br>
+• &ensp;	Pasien dengan gejala pernapasan ringan yang dapat ditangani di rumah, termasuk wisatawan yang kembali dengan penyakit dalam waktu 14 hari setelah kembali dari tempat positif terjangkit virus.
+</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="container5">
+            <div class="isi">
+                <div class="head">
+                <h4>GEJALA</h4>
+                </div>
+                <p>&ensp; Gejala awal infeksi virus Corona atau COVID-19 bisa menyerupai gejala flu, yaitu demam, pilek, batuk kering, sakit tenggorokan, dan sakit kepala. Setelah itu, gejala dapat hilang dan sembuh atau malah memberat. Penderita dengan gejala yang berat bisa mengalami demam tinggi, batuk berdahak bahkan berdarah, sesak napas, dan nyeri dada. Gejala-gejala tersebut muncul ketika tubuh bereaksi melawan virus Corona. <br> <br>
+Secara umum, ada 3 gejala umum yang bisa menandakan seseorang terinfeksi virus Corona, yaitu: <br>
+•	&ensp; Demam (suhu tubuh di atas 38 derajat Celsius) <br>
+•	&ensp; Batuk <br>
+•	&ensp; Sesak napas <br>
+<br>
+Gejala-gejala COVID-19 ini umumnya muncul dalam waktu 2 hari sampai 2 minggu setelah penderita terpapar virus Corona.
+</p>
+            </div>
+            <div class="isi">
+                <div class="head">
+                <h4>PENCEGAHAN</h4>
+                </div>
+                <p>•	&ensp; Terapkan physical distancing, yaitu menjaga jarak minimal 1 meter dari orang lain, dan jangan dulu ke luar rumah kecuali ada keperluan mendesak. <br>
+•	&ensp; Gunakan masker saat beraktivitas di tempat umum atau keramaian, termasuk saat pergi berbelanja bahan makanan. <br>
+•	&ensp; Rutin mencuci tangan dengan air dan sabun atau hand sanitizer yang mengandung alkohol minimal 60%, terutama setelah beraktivitas di luar rumah atau di tempat umum. <br>
+•	&ensp; Jangan menyentuh mata, mulut, dan hidung sebelum mencuci tangan. <br>
+•	&ensp; Tingkatkan daya tahan tubuh dengan pola hidup sehat. <br>
+•	&ensp; Hindari kontak dengan penderita COVID-19, orang yang dicurigai positif terinfeksi virus Corona, atau orang yang sedang sakit demam, batuk, atau pilek. <br>
+•	&ensp; Tutup mulut dan hidung dengan tisu saat batuk atau bersin, kemudian buang tisu ke tempat sampah. <br>
+•	&ensp; Jaga kebersihan benda yang sering disentuh dan kebersihan lingkungan, termasuk kebersihan rumah. <br>
+
+</p>
+            </div>
+            <a class="btn" href="https://www.who.int/indonesia/news/novel-coronavirus/qa-for-public">SELENGKAPNYA</a>
+        </div>
+        <div class="profil-area" id="profil">
+            <div class="container6 ">
+                <h2>ABOUT ME</h2>
+                <div class="card2">
+                    <div class="av">
+                        <img src="img/av.png" alt="">
+                    </div>
+                    <div class="isi2">
+                    <h4>Hai, Saya Genta Bhatara</h4>
+                    <p>
+                    Seorang remaja umur 16 tahun asal Tulungagung, saat ini bersekolah di SMK TELKOM MALANG kelas 10 RPL 1 <br> <br>
+                    Find Me :
+                    </p>
+                    <div class="sosmed">
+                        <a href="https://www.instagram.com/geteeeee/" class="fab fa-instagram"></a>
+                        <a href="https://twitter.com/geteee_" class="fab fa-twitter"></a>
+                        <a href="https://www.facebook.com/genta.bhatara.5" class="fab fa-facebook-f"></a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="footer">
+        <div class="container7">
+            <h3>TERIMA KASIH!</h3>
+        </div>
+    </div>
+</body>
+</html>
