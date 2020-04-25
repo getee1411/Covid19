71944627cde5ee36935bb4c37908e1c1ffe3bbeb
getee1411 <62593648+getee1411@users.noreply.github.com>
2020-04-25 09:24:32 +0700

    tampilan akhir

---
+++ end.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HASIL TES</title>
+
+    <link rel="stylesheet" href="../css/style3.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="data">
+            <H2>DATA BERHASIL DIKIRIM</H2>
+            <h3>silahkan menunggu untuk hasil lebih lanjut</h3>
+            <p>Tetap jaga kesehatan, patuhi aturan pemerintah, dan #stayhome</p>
+            <div class="link">
+                <a class="btn" href="../index.php">Kembali Ke Beranda</a>
+                <a class="btn" href="../start.php">Check Up Lagi</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
